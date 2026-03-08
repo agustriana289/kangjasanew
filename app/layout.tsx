66697bd1228f4e02ed2be3855c523bad207117b3
Agus Triana <agustriana289@gmail.com>
2026-03-08 12:03:26 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -86,7 +86,6 @@ export default async function RootLayout({
           `
         }} />
 
-        {/* Inject Custom Header HTML dari settings dengan aman */}
         {customHeaderHtml && (
           <script
             dangerouslySetInnerHTML={{
